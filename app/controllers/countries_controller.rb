01e9d0d3f67c89abf9e45cadead60de309647434
yutian zheng <yutian.zheng@mail.mcgill.ca>
2017-07-18 14:09:58 -0400

    cities controller added

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,26 @@
 class CountriesController < ApplicationController
+
+  def index
+     @countries = Country.all
+  end
+
+
+  def new
+    @country = Country.new
+  end
+
+  def create
+    @country = Country.new(country_params)
+    @country.save
+  end
+
+  def show
+    @city = City.find(params[:id])
+  end
+
+  private
+
+  def country_params
+      params.require(:county).permit(:name)
+  end
 end
