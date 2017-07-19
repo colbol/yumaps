cbe11fc0d7fa6bdc79ee7454d706b629a7b5e0c6
Salim Chorfi <salimchorfi@gmail.com>
2017-07-19 14:53:03 -0400

    Added the simpleForm in the map pop Up

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,7 @@
 class CitiesController < ApplicationController
   def index
      @cities = City.all
+     @tag = Tag.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :countries, only: [:index, :new, :create, :show ]
   resources :cities, only: [:index, :new, :create, :show ]
   root to: 'pages#home'
+  resources :tags, only: [:create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
