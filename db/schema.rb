0c013106817058738d7e8e299cd916460b45b1e1
Salim Chorfi <salimchorfi@gmail.com>
2017-07-20 14:25:40 -0400

    Added the information from the GeoJSON file to the Tags params coming from the simple form

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719211912) do
-
+ActiveRecord::Schema.define(version: 20170720180510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,9 +55,11 @@ ActiveRecord::Schema.define(version: 20170719211912) do
   create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.boolean  "display"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.integer  "district_id"
+    t.string   "content"
+    t.integer  "district_index"
     t.index ["district_id"], name: "index_tags_on_district_id", using: :btree
   end
 
