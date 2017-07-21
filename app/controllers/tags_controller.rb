fe51eb0767edafe5e1d3806504bbbb82e60d5cd8
Salim Chorfi <salimchorfi@gmail.com>
2017-07-21 11:31:49 -0400

    Modified the Tag controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,16 +3,16 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
 
-    def update
-      @tag = Vote.find(params[:id])
-      if params[:type] == "upvote"
-        @vote.countdown = @vote.count + 1
-      else
-        @vote.countdown = @vote.countdown - 1
-      end
-      if @vote.save
-        respond_to do |format|
-          format.html { redirect_to vote_path(@vote) }
+  def update
+    @tag = Vote.find(params[:id])
+    if params[:type] == "upvote"
+      @vote.countdown = @vote.count + 1
+    else
+      @vote.countdown = @vote.countdown - 1
+    end
+    if @vote.save
+      respond_to do |format|
+        format.html { redirect_to vote_path(@vote) }
         format.js  # <-- will render `app/views/votes/update.js.erb`
       end
     else
@@ -25,11 +25,17 @@ class TagsController < ApplicationController
 
 
   def create
-    @tag= Tag.new(tag_params)
-    @tag.save
-  end
+    @tag = Tag.find_by({content: params[:tag][:content], district_index: params[:tag][:district_index]})
+    if @tag.nil?
+     @tag = Tag.new(tag_params)
+     @tag.district = District.find_by(name: params[:tag][:name])
+     @tag.save
+   end
+   Vote.find_or_create_by({tag_id: @tag.id, ip: request.remote_ip})
+   redirect_to cities_path
+ end
 
-  private
+ private
 
   def tag_params
     params.require(:tag).permit(:name, :district_index, :content)
