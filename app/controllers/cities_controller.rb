887652bdac9337cc0a90e6640b91dc52368d271c
Salim Chorfi <salimchorfi@gmail.com>
2017-07-24 10:58:03 -0400

    Lot's of changes

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,8 +1,11 @@
+require 'gon'
+
 class CitiesController < ApplicationController
   def index
-     @cities = City.all
-     @tag = Tag.new
+    @cities = City.all
+    @tag = Tag.new
   end
+
 
   def new
     @city = City.new
@@ -14,7 +17,6 @@ class CitiesController < ApplicationController
   end
 
   def update
-
     @city = City.find(params[:id])
 
     @ciy.update_attributes(user_params)
@@ -26,11 +28,43 @@ class CitiesController < ApplicationController
     @city = City.find(params[:id])
     @tag = Tag.new
     @tags = Tag.all
+    @votes = Vote.all
+    @name = fetch_name
+
+    @district_tag = []
+    @data = {}
+    tag_vote = []
+
+    @tags.each do |tags|
+       @district_tag.push(tags) if tags['name'] == @name
+    end
+
+    @district_tag.each do |tag|
+      @data[tag.content] = tag.votes.count
+    end
+    @top = @data.sort_by { |content, votes| - votes }
+    gon.hello = "hello"
   end
 
   def fetch_name
-    name = params[:name]
-    @name = name
+    @name = params[:name]
+    @tags = Tag.all
+
+    @district_tag = []
+    @top = []
+    tag_vote = []
+
+    @tags.each do |tags|
+       @district_tag.push(tags) if tags['name'] == @name
+    end
+    # check if really most popular 20 tags
+    @district_tag.sort { |a, b| a.votes.count <=> b.votes.count }.first(20).each do |tag|
+      data = {}
+      data[:name] = tag.content
+      data[:votes] = tag.votes.count
+      @top << data
+    end
+    @top = @top.reverse
   end
 
   private
