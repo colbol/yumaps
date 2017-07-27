89a3e5d08a4d3335b7afab03e5afddf49dfbc887
yutian zheng <yutian.zheng@mail.mcgill.ca>
2017-07-27 13:56:15 -0400

    city id fixed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -12,30 +12,8 @@ class VotesController < ApplicationController
     @vote.save!
   end
 
-  def update
-    @vote = Vote.find(params[:id])
-    if params[:type] == "upvote"
-      @vote.countdown = @vote.count + 1
-    else
-      @vote.countdown = @vote.countdown - 1
-    end
-    if @vote.save
-      respond_to do |format|
-        format.html { redirect_to vote_path(@vote) }
-        format.js  # <-- will render `app/views/votes/update.js.erb`
-      end
-    else
-      respond_to do |format|
-        format.html { render 'votes/index' }
-        format.js  # <-- idem
-      end
-    end
- end
-
-
-
   def show
-        @vote = Vote.find(params[:id])
+    @vote = Vote.find(params[:id])
   end
 
     private
